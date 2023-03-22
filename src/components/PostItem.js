739fbc384dadd8af5b6d644d46bcc7bf0caeb082
SerhiiChynin <sergey.chinin@gmail.com>
2023-03-22 21:58:15 +0200

    all work

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { removePostById } from '../slicers/post/postSlice'
+import { useDispatch } from 'react-redux'
 
 const PostItem = ({ post }) => {
+    const dispatch = useDispatch();
+
     return (
-        <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'>
+        <div
+        onClick={() => dispatch(removePostById(post.id))}
+            className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'>
             {post.title}
         </div>
     )
--- src/slicers/post/postSlice.js
+++ src/slicers/post/postSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import {axios} from 'axios';
+import axios from 'axios';
 
 
 const initialState = {
@@ -7,9 +7,18 @@ const initialState = {
 }
 export const getPosts = createAsyncThunk(
     'posts/getPosts', //имя состоящие из имени слайса и название константы через слеш
-    async (_,{rejectWithValue, dispatch}) => {
-        const res = await axios.get('https://jsonplaceholder.typicode.com/posts') 
+    async (_, { rejectWithValue, dispatch }) => {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
         dispatch(setPosts(res.data))
+        console.log(res.data);
+    }
+);
+
+export const removePostById = createAsyncThunk(
+    'posts/removePostById',
+    async (id, {rejectWithValue, dispatch}) => {
+        await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        dispatch(removePosts(id))
     }
 )
 
@@ -17,17 +26,27 @@ export const postSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        setPosts: (state, action) => {
-            console.log((state.posts));
+        setPosts: (state, action) => {            
+            const half = action.payload.filter(item => item.id % 2 === 0);
             state.posts = action.payload;
+            // state.posts = half;
+
         },
+        removePosts: (state, action) => {
+            console.log(action);
+            state.posts = state.posts.filter((post) => post.id !== action.payload);
+        }
     },
     extraReducers: {
-        [getPosts.fulfilled]: () => console.log('fullfiald'),
-        [getPosts.pending]: () => console.log('pending'),
-        [getPosts.rejected]: () => console.log('rejected'),
+        [getPosts.fulfilled]: () => console.log('getPost: fulfilled'),
+        [getPosts.pending]: () => console.log('getPost: pending'),
+        [getPosts.rejected]: () => console.log('getPost: rejected'),
+        [removePostById.fulfilled]: () => console.log('removePost: fulfilled'),
+        [removePostById.pending]: () => console.log('removePost: pending'),
+        [removePostById.rejected]: () => console.log('removePost: rejected'),
+        
     }
 })
 
-export const { setPosts } = postSlice.actions;
+export const { setPosts, removePosts } = postSlice.actions;
 export default postSlice.reducer;
